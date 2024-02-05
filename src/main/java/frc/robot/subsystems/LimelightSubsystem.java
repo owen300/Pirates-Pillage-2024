080969b480735db5447fb187bd331bf8b61e6b1f
= <=>
2024-02-05 13:09:58 -0500

    LimelightSubsystem 2/05/24-13:09

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -1,12 +1,16 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
-public class LimelightSubsystem {
-    
+public class LimelightSubsystem extends SubsystemBase {
     NetworkTable table; 
     NetworkTableEntry tx;
     NetworkTableEntry ty;
@@ -36,9 +40,11 @@ public class LimelightSubsystem {
     SmartDashboard.putNumber("LimelightY", getTy());
     SmartDashboard.putNumber("LimelightArea", getTa());
    }
+   
+  @Override
+  public void periodic() {
+    displayData();
+  }
 
-
-
-
-
+ 
 }
