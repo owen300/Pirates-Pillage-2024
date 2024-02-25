f49a526fcd8d5b2e778522255ab5801231a875f2
= <=>
2024-02-25 15:14:03 -0500

    intake current detection code ready to test

--- src/main/java/frc/robot/commands/ScoreCommands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/ScoreCommands/IntakeCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.ScoreCommands;
 
 import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.EndEffectorSubsystem;
 import static frc.robot.Constants.SubsystemConstants;
@@ -10,6 +11,7 @@ public class IntakeCommand extends Command{
     private final EndEffectorSubsystem intakeSubsystem;
     private final double speed;
     private final boolean inverted; 
+     boolean noteIn = false; 
     private Debouncer filter_debouncer = new Debouncer(SubsystemConstants.kDebounceTime, Debouncer.DebounceType.kBoth);
 
     public IntakeCommand(EndEffectorSubsystem intakeSubsystem, double speed, boolean inverted){
@@ -21,24 +23,31 @@ public class IntakeCommand extends Command{
 
     @Override 
     public void initialize(){
+      SmartDashboard.putBoolean("NoteIn", noteIn); 
       intakeSubsystem.setIntakeSpeedDirection(speed, inverted);
     }
 
-    // @Override 
-    // public boolean isFinished(){ 
-    //     if(SubsystemConstants.kDebounce) {
-    //         return filter_debouncer.calculate(EndEffectorSubsystem.filteredCurrentIntake>SubsystemConstants.kCurrentThreshold);
-    //     } else {
-    //         return EndEffectorSubsystem.filteredCurrentIntake > SubsystemConstants.kCurrentThreshold;
-    //     }
-    // }
-    // @Override
-    // public void end(boolean interrupted){
-    //     if(interrupted) {
-    //         intakeSubsystem.setIntakeSpeedDirection(0.05, false); 
-    //     }
-    //     else {
-    //         intakeSubsystem.setIntakeSpeedDirection(0.05, false);
-    //     }
-    // }
+    @Override 
+    public boolean isFinished(){ 
+        if(SubsystemConstants.kDebounce) {
+            noteIn = true; 
+            SmartDashboard.putBoolean("NoteIn", noteIn); 
+            return filter_debouncer.calculate(EndEffectorSubsystem.filteredCurrentIntake>SubsystemConstants.kCurrentThreshold);
+        } else {
+            noteIn = true; 
+            SmartDashboard.putBoolean("NoteIn", noteIn); 
+            return EndEffectorSubsystem.filteredCurrentIntake > SubsystemConstants.kCurrentThreshold;
+        }
+    }
+    @Override
+    public void end(boolean interrupted){
+      if(interrupted){
+        if((EndEffectorSubsystem.filteredCurrentIntake < SubsystemConstants.kCurrentThreshold) && noteIn) {
+            intakeSubsystem.setIntakeSpeedDirection(0, inverted); 
+        }
+        else {
+            intakeSubsystem.setIntakeSpeedDirection(0, inverted);
+        }
+      }
+    }
 }
