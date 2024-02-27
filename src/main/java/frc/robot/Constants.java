6262bb2233147cf7a037698a9bc3958083dd86f2
= <=>
2024-02-26 22:33:15 -0500

    intake cur

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -129,7 +129,7 @@ public final class Constants {
     public static final int kIntakeSampleSize = 5; 
     public static final double kIntakeCurrentThreshold = 13.5; 
     public static final boolean kIntakeDebounce = false; 
-    public static final double kIntakeDebounceTime = 0.05; 
+    public static final double kIntakeDebounceTime = 1; 
 
     public static final int kShootLead = 14; 
     public static final int kShootFollow = 13; 
