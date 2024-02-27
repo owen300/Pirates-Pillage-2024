e0c5671970510529aee3c75674fe8b2a8f9e46ad
= <=>
2024-02-27 18:08:03 -0500

    set points

--- src/main/java/frc/robot/commands/ScoreCommands/ScoreCommandHolder.java
+++ src/main/java/frc/robot/commands/ScoreCommands/ScoreCommandHolder.java
@@ -61,11 +61,11 @@ public class ScoreCommandHolder extends Command {
     }
 
     public Command liftScoreAmp(){
-        return new LiftCommand(endEffectorSubsystem, 21); 
+        return new LiftCommand(endEffectorSubsystem, 0.08401); 
     }
 
     public Command liftScoreSpeaker(){
-        return new LiftCommand(endEffectorSubsystem, 1.5); 
+        return new LiftCommand(endEffectorSubsystem, 0.09861); 
     }
 
 
