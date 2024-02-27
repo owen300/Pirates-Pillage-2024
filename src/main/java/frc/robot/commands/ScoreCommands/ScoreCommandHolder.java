c74d8143a96458f576aefd9644f821de5931a820
= <=>
2024-02-26 19:56:35 -0500

    test lift

--- src/main/java/frc/robot/commands/ScoreCommands/ScoreCommandHolder.java
+++ src/main/java/frc/robot/commands/ScoreCommands/ScoreCommandHolder.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.ScoreCommands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.EndEffectorSubsystem;
@@ -24,7 +25,7 @@ public class ScoreCommandHolder extends Command {
     }
 
     public Command testLift(){
-        return new LiftCommand(endEffectorSubsystem, 4); 
+        return new LiftCommand(endEffectorSubsystem, 18.7); 
     }
 
 
