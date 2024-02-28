08fd16309f7dfc8521aa432765f4192fa17a57f3
= <=>
2024-02-27 19:55:39 -0500

    current test

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -127,7 +127,7 @@ public final class Constants {
   public static final class SubsystemConstants {
     public static final int kIntakeMotorCANID = 15; 
     public static final int kIntakeSampleSize = 5; 
-    public static final double kIntakeCurrentThreshold = 14.5; 
+    public static final double kIntakeCurrentThreshold = 14.3; 
     public static final boolean kIntakeDebounce = false; 
     public static final double kIntakeDebounceTime = 0; 
 
