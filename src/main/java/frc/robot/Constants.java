ac4fac368e85671fec3a278ab3ca2cb091691e7f
= <=>
2024-02-27 20:28:38 -0500

    test cur

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -128,8 +128,8 @@ public final class Constants {
     public static final int kIntakeMotorCANID = 15; 
     public static final int kIntakeSampleSize = 5; 
     public static final double kIntakeCurrentThreshold = 7; 
-    public static final boolean kIntakeDebounce = true; 
-    public static final double kIntakeDebounceTime = 0.7; 
+    public static final boolean kIntakeDebounce = false; 
+    public static final double kIntakeDebounceTime = 0.5; 
 
     public static final int kShootLead = 14; 
     public static final int kShootFollow = 13; 
